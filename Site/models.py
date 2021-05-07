48db3ff7d461edbecbf81b25755ed0d6899c7bce
rafal1999 <ravstb4@gmail.com>
2021-05-07 12:53:39 +0200

    TEST

--- Site/models.py
+++ Site/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Candidates(models.Model):
+    ID_candidate = models.AutoField(primary_key=True)
+    Name = models.TextField(default="")
+    Surname = models.TextField()
+    def __str__(self): #musi być __str__ bo inaczej nie działa tylko wyświetla niapis że obiekt jest 
+        return self.Name
+
+    def __str__(self): #musi być __str__ bo inaczej nie działa tylko wyświetla niapis że obiekt jest 
+        return self.Surname
--- Site/views.py
+++ Site/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http      import HttpResponse  
+from django.db.models.functions import Concat 
+from django.db.models import F, Value
+from Site.models import Candidates 
 # Create your views here.
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    
+    if request.method=="POST":
+        Candidates.objects.create(Name=request.POST['candidate_name'])#,Surname=request.POST[])
+
+    candidates = Candidates.objects.all()
+    # print("____________________________________________")
+    # print(candidates)
+    # print("____________________________________________")
+    return render(request,'home.html', {'candidates':candidates})
 
