7c3b1ce7b15d729cca3dacd66685d5770ed90db0
rafal1999 <ravstb4@gmail.com>
2021-05-16 19:03:35 +0200

    prawie pełna tabela kandydatów i ich ról

--- Site/models.py
+++ Site/models.py
@@ -1,13 +1,36 @@
 from django.db import models
 
-
+class Candidates_Role(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.TextField(default="")
 
 class Candidates(models.Model):
-    ID_candidate = models.AutoField(primary_key=True)
-    Name = models.TextField(default="")
-    Surname = models.TextField()
+    ID = models.AutoField(primary_key=True)
+    Name = models.TextField(default="",null=True)
+    Surname = models.TextField(default="",null=True)
+    Birthdate = models.DateField(auto_now=False, auto_now_add=False)
+    Application_date = models.DateField(auto_now=False, auto_now_add=True)
+    Phone_number=models.CharField(max_length=13, null=True) # istnije takie cos jak phone number 
+    Sex = models.CharField(max_length=1, null=True)
+    Email_addres = models.EmailField(max_length=50,null=True)
+    CV= models.TextField(default="")
+    Motivation_letter = models.TextField(default="")
+    Stage = models.CharField(max_length=1,default='1') #!
+    Hired = models.CharField(max_length=1,default='P') # T - zatrudniony N -niezatrudniony P - in proces 
+    ID_Candidates_Role = models.ForeignKey('Candidates_Role',default=None,null=True, on_delete= models.SET_NULL)  
+
+
+    # CV= models.FilePathField(unique=True,path=None, match=None, recursive=recursive, max_length=200)
+
+   
     def __str__(self): #musi być __str__ bo inaczej nie działa tylko wyświetla niapis że obiekt jest 
         return self.Name
-
-    def __str__(self): #musi być __str__ bo inaczej nie działa tylko wyświetla niapis że obiekt jest 
+    def __str__(self):
         return self.Surname
+    def __str__(self):
+        return self.Application_date
+    def __str__(self):
+        return self.Birthdate
+    def __str__(self):
+        return self.Phone_number
+    
--- Site/views.py
+++ Site/views.py
@@ -3,13 +3,14 @@ from django.http      import HttpResponse
 from django.db.models.functions import Concat 
 from django.db.models import F, Value
 from Site.models import Candidates 
+import datetime
 # Create your views here.
 
 
 def home_page(request):
     
     if request.method=="POST":
-        Candidates.objects.create(Name=request.POST['candidate_name'], Surname =request.POST['candidate_surname'])
+        Candidates.objects.create(Name=request.POST['candidate_name'], Surname =request.POST['candidate_surname'],Birthdate=datetime.date(1999,12,11))
 
     candidates = Candidates.objects.all()
     
