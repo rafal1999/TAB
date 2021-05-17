270a327dbff73d794c9abbde47be9038264e8ce0
rafal1999 <ravstb4@gmail.com>
2021-05-17 17:29:06 +0200

    tabele Workers i Workers_Role

--- Site/models.py
+++ Site/models.py
@@ -18,11 +18,7 @@ class Candidates(models.Model):
     Stage = models.CharField(max_length=1,default='1') #!
     Hired = models.CharField(max_length=1,default='P') # T - zatrudniony N -niezatrudniony P - in proces 
     ID_Candidates_Role = models.ForeignKey('Candidates_Role',default=None,null=True, on_delete= models.SET_NULL)  
-
-
     # CV= models.FilePathField(unique=True,path=None, match=None, recursive=recursive, max_length=200)
-
-   
     def __str__(self): #musi być __str__ bo inaczej nie działa tylko wyświetla niapis że obiekt jest 
         return self.Name
     def __str__(self):
@@ -33,4 +29,17 @@ class Candidates(models.Model):
         return self.Birthdate
     def __str__(self):
         return self.Phone_number
-    
+
+class Workers_Role(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.TextField(default="")
+class Workers(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.TextField(default="",null=True)
+    Surname = models.TextField(default="",null=True)
+    Birthdate = models.DateField(auto_now=False, auto_now_add=False,null=True)
+    Login = models.CharField(max_length=32,null=True)
+    Password = models.CharField(max_length=32,null=False,default="")
+    Email_addres = models.EmailField(max_length=50,null=True)
+    ID_Workers_Role = models.ForeignKey("Workers_Role", default=None, null=True, on_delete=models.SET_NULL)
+
--- Site/views.py
+++ Site/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from django.http      import HttpResponse  
+from django.shortcuts           import render
+from django.http                import HttpResponse  
 from django.db.models.functions import Concat 
-from django.db.models import F, Value
-from Site.models import Candidates 
-import datetime
+from django.db.models           import F, Value
+from Site.models                import Candidates, Workers 
+from datetime                   import date
 # Create your views here.
 
 
 def home_page(request):
     
     if request.method=="POST":
-        Candidates.objects.create(Name=request.POST['candidate_name'], Surname =request.POST['candidate_surname'],Birthdate=datetime.date(1999,12,11))
+        Candidates.objects.create(Name=request.POST['candidate_name'], Surname =request.POST['candidate_surname'],Birthdate=date(1999,12,11))
 
-    candidates = Candidates.objects.all()
+    candidates = Workers.objects.all()# tabele się łączą automatycznie jeśli mają powiązanie,
     
     return render(request,'home.html', {'candidates':candidates})
 
