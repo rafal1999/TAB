1a0132a7b7064992d0075e5eaec5505927eadea5
rafal1999 <ravstb4@gmail.com>
2021-09-09 20:47:48 +0200

    test

--- Site/views.py
+++ Site/views.py
@@ -176,3 +176,84 @@ class calendar(LoginRequiredMixin, View):
             worker = WorkersAPI.get_worker_by_user_id(request.user.id)
         selected = 0
         return render(request, self.template, {'meetings':meetings, 'selected':selected, 'worker':worker}) 
+
+
+
+
+
+def edit_candidate_page(request,id_candidate):
+
+    if(request.method=='POST'):
+        edit_candidate(id=id_candidate,name=request.POST['candidate_name'],surname=request.POST['candidate_surname'], 
+                        birthday=request.POST['candidate_birthdate'], phone_number=request.POST['candidate_phone_number'],
+                        sex=request.POST['candidate_sex'], email=request.POST['candidate_email'],
+                        cv=request.POST['candidate_cv'],motivation_letter=request.POST['candidate_motivation_letter'],)
+        candidate=Candidates.objects.get(ID=id_candidate)
+        candidate.Birthdate =candidate.Birthdate.strftime("%Y-%m-%d") 
+        return render(request,'editcandidate.html',{'candidate':candidate})
+   
+    candidate=Candidates.objects.get(ID=id_candidate)
+    candidate.Birthdate =candidate.Birthdate.strftime("%Y-%m-%d")
+    return render(request,'editcandidate.html',{'candidate':candidate})
+
+def add_interview_data_page(request,id_process):
+
+    if (check_if_interview_took_place(id_process=id_process)):
+        return redirect('interview_summary_page',id_process=id_process)
+   
+    if(request.method=='POST'):
+        add_interview_data(id_process=id_process, id_worker=1, hard_skils=request.POST['hard_skils'], 
+                            soft_skils=request.POST["soft_skils"], grade=request.POST["grade"], notes=request.POST['notes']) 
+        return redirect(interview_summary_page,id_process=id_process)
+
+    process = Recruitment_Process.objects.get(ID=id_process)
+    candidate=Candidates.objects.get(ID=process.ID_Candidates.ID)
+    role = Candidates_Role.objects.get(ID=process.ID_Candidates_Role.ID)
+
+    return render(request, 'interview.html',{'candidate':candidate,'role':role})
+
+
+def interview_summary_page(request, id_process):
+
+    process = Recruitment_Process.objects.get(ID=id_process)
+    candidate=Candidates.objects.get(ID=process.ID_Candidates.ID)
+    role = Candidates_Role.objects.get(ID=process.ID_Candidates_Role.ID)   
+    interview_data = Recruitment_Meetings.objects.get(ID_Recruitment_Process=process)
+    
+    return render(request,"interview_summary.html",{'candidate':candidate,'role':role,
+                'interview_data': interview_data})
+
+def assistant_page(request):
+# 
+    if(request.method=='POST'):
+        if(request.POST['form_type']=='addform'):
+           return  redirect('add_candidate_page') 
+        if(request.POST['form_type']=='editform'):
+           return  redirect('edit_candidate_page',id_candidate=int(request.POST["candidate"]))
+        if(request.POST['form_type']=='processform'):
+           return redirect('add_process_page')
+
+    candidates = Candidates.objects.all() 
+    return render(request,"assistant.html",{'candidates':candidates})
+
+def add_candidate_page(request):
+    if request.method=="POST":
+        add_candidate(name=request.POST['candidate_name'],surname=request.POST['candidate_surname'], 
+                        birthday=request.POST['candidate_birthdate'], phone_number=request.POST['candidate_phone_number'],
+                        sex=request.POST['candidate_sex'], email='test@m.com',cv='pass',motivation_letter='pas',
+                        hired='P')
+        return  redirect('assistant_page')
+
+    return render(request,"addcandidate.html") 	
+
+def add_process_page(request):
+
+    candidates=list_candidates()
+    roles=list_candidates_roles()
+    if(request.method=='POST'):
+        add_process(id_candidate=request.POST['candidate'], id_role=request.POST['role'])
+
+    return render(request,'addprocess.html',{'candidates':candidates, 'roles':roles})
+
+def process_sumary_page(request):
+    pass
